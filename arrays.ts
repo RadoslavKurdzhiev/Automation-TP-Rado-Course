50ce838dee2542c7227ea4f6c6ba82f64cf3eb6f
rkyurdzhiev <rkyurdzhiev@tirebuyer.com>
2025-10-16 19:56:10 +0300

    array methods

--- arrays.ts
+++ arrays.ts
@@ -25,3 +25,32 @@ let checkTypeOf = combinedArray.map((element: number | string) => {
 	}
 });
 console.log(checkTypeOf);
+//-----------------------------
+//Method filter
+let positiveNumbers: number[] = [2, 3, 4, 5, 6];
+
+let filteredArray: number[] = positiveNumbers.filter((num: number) => num > 3);
+console.log(filteredArray);
+
+//method sort
+let sortNumbers: number[] = [100, 2, 30, 5, 40, 6];
+let sortedNumbers: number[] = sortNumbers.sort(
+	(a: number, b: number) => a - b
+);
+console.log(sortedNumbers);
+
+//method push
+sortedNumbers.push(200);
+console.log(sortedNumbers);
+
+//method pop
+sortedNumbers.pop();
+console.log(sortedNumbers);
+
+//method shift
+sortedNumbers.shift();
+console.log(sortedNumbers);
+
+//method unshift
+sortedNumbers.unshift(1);
+console.log(sortedNumbers);
