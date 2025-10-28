7889afe4b93d86eefa6bf7832f0fd52457582b07
rkyurdzhiev <rkyurdzhiev@tirebuyer.com>
2025-10-28 19:02:40 +0200

    add new exercises

--- basic-types/basic-types.ts
+++ basic-types/basic-types.ts
@@ -17,15 +17,15 @@ console.log(messageLength);
 let welcomeMessageDetails: string = `Text message length is ${messageLength}`;
 console.log(welcomeMessageDetails);
 
-// number and number operations 
+// number and number operations
 
 let numOne: number = 10;
 let numTwo: number = 5;
 
 let add: number = numOne + numTwo;
 let substract: number = numTwo - numTwo;
-let multiply: number = numOne*numTwo;
-let divide: number = numOne/numTwo;
+let multiply: number = numOne * numTwo;
+let divide: number = numOne / numTwo;
 
 //boolean (true/false)
 // === -> проверява дали са равни стойността и типа на променливите
@@ -42,18 +42,22 @@ let isAnyNumEqualTo: boolean = numOne === 10 || numTwo === 5;
 
 // ternary operator
 //let test = 5 === 5 ? 'true' : 'false';
-let positiveNumComparison: string = numOne > 0 ? `The number ${numOne} is positive` : `The number ${numOne} is negative`;
+let positiveNumComparison: string =
+	numOne > 0
+		? `The number ${numOne} is positive`
+		: `The number ${numOne} is negative`;
 console.log(positiveNumComparison);
-
 
 // if/else statement
 
 const numThree: number = 100;
 
 if (numThree <= 10) {
-    console.log("The number is less or equal to 10");
+	console.log('The number is less or equal to 10');
 } else if (numThree > 50) {
-    console.log("The number is greater than 50");
+	console.log('The number is greater than 50');
 } else {
-    console.log("numOne is not greater than 3");
+	console.log('numOne is not greater than 3');
 }
+
+//  union type
