bb1756641a1b0c34a8c826dcf521f4b0fc496f45
rkyurdzhiev <rkyurdzhiev@tirebuyer.com>
2025-10-30 19:39:05 +0200

    exercise async await

--- async-await.ts
+++ async-await.ts
@@ -77,11 +77,43 @@ getData();
 
 //try catch finally
 try {
-    // parse json to js object
-    const n = JSON.parse('{"valid": true}');
-    console.log("OK:", n.valid);
+	// parse json to js object
+	const n = JSON.parse('{"valid": true}');
+	console.log('OK:', n.valid);
 } catch (err) {
-    console.error("Invalid JSON!", err);
+	console.error('Invalid JSON!', err);
 } finally {
-    console.log("Ready");
+	console.log('Ready');
 }
+
+// try/catch/finally exercise
+
+function divideNumbers(numOne: number, numTwo: number) {
+	try {
+		if (numTwo === 0) {
+			throw new Error('Cannot devide by zero!');
+		} else {
+			const result: number = numOne / numTwo;
+			console.log(`Result ${result}`);
+		}
+	} catch (err) {
+		console.error(`Error:${err}`);
+	} finally {
+		console.log('Operation End');
+	}
+}
+
+divideNumbers(20, 2);
+divideNumbers(20, 0);
+
+async function getDataSecond(): Promise<void> {
+	try {
+		const response = await fetch('https://fakestoreapi.com/products/1');
+		const data: Product = await response.json();
+		console.log(data);
+	} catch (error: unknown) {
+		console.error(error);
+	}
+}
+
+getDataSecond();
